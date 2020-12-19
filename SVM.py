ae398ab2c7b051bd33b9af01cf0e47d46c22e73b
manika <smakam@umd.edu>
2020-12-19 15:03:37 -0500

    final

--- SVM.py
+++ SVM.py
@@ -19,12 +19,14 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
 
 
-def svm_model(X_train, y_train, X_test, y_test, kernel,transform,C, gamma):
+def svm_model(X_train, y_train, X_test, y_test, kernel,transform,C, gamma, X_test_orig):
 
 	if C == 'default' and gamma == 'default':
 		model = SVC(kernel=kernel)
 	else:
 		model = SVC(C = C,gamma= gamma, kernel=kernel)
+
+	# print(X_train.shape)
 
 	model.fit(X_train, y_train)
 
@@ -51,7 +53,9 @@ def svm_model(X_train, y_train, X_test, y_test, kernel,transform,C, gamma):
 	plt.savefig(f'transform={transform}_kernel={kernel}_C={C}_gamma={gamma}_CM.png')
 	plt.close()
 
-	X_test__ = X_test.reshape(X_test.shape[0], 28, 28)
+	#plot predictions
+	# X_test__ = X_test.reshape(X_test.shape[0], int(np.sqrt(X_test.shape[1])),int(np.sqrt(X_test.shape[1])))
+	X_test__ = X_test_orig.reshape(X_test_orig.shape[0], 28, 28)
 
 	fig, axis = plt.subplots(4, 4, figsize=(12, 14))
 	for i, ax in enumerate(axis.flat):
@@ -61,86 +65,6 @@ def svm_model(X_train, y_train, X_test, y_test, kernel,transform,C, gamma):
 	plt.savefig(f'transform={transform}_kernel={kernel}_C={C}_gamma={gamma}_results.png')
 	plt.close()
 
-
-def hyperparameter_tuning(X_train, y_train, X_test, y_test, kernel):
-	# creating a KFold object with 5 splits 
-	folds = KFold(n_splits = 5, shuffle = True, random_state = 10)
-
-	# specify range of hyperparameters
-	# Set the parameters by cross-validation
-	hyper_params = [ {'gamma': [1e-2, 1e-3, 1e-4],
-	                     'C': [5,10]}]
-
-
-	# specify model
-	model = SVC(kernel=kernel)
-
-	# set up GridSearchCV()
-	model_cv = GridSearchCV(estimator = model, 
-	                        param_grid = hyper_params, 
-	                        scoring= 'accuracy', 
-	                        cv = folds, 
-	                        verbose = 1,
-	                        return_train_score=True)      
-
-	# fit the model
-	print("hereeeeee")
-	model_cv.fit(X_train, y_train)
-	print("doneeee")
-
-	cv_results = pd.DataFrame(model_cv.cv_results_)
-
-	# converting C to numeric type for plotting on x-axis
-	cv_results['param_C'] = cv_results['param_C'].astype('int')
-
-	# # plotting
-	plt.figure(figsize=(16,8))
-
-	# subplot 1/3
-	plt.subplot(131)
-	gamma_01 = cv_results[cv_results['param_gamma']==0.01]
-
-	plt.plot(gamma_01["param_C"], gamma_01["mean_test_score"])
-	plt.plot(gamma_01["param_C"], gamma_01["mean_train_score"])
-	plt.xlabel('C')
-	plt.ylabel('Accuracy')
-	plt.title("Gamma=0.01")
-	plt.ylim([0.60, 1])
-	plt.legend(['test accuracy', 'train accuracy'], loc='upper left')
-	plt.xscale('log')
-
-	# subplot 2/3
-	plt.subplot(132)
-	gamma_001 = cv_results[cv_results['param_gamma']==0.001]
-
-	plt.plot(gamma_001["param_C"], gamma_001["mean_test_score"])
-	plt.plot(gamma_001["param_C"], gamma_001["mean_train_score"])
-	plt.xlabel('C')
-	plt.ylabel('Accuracy')
-	plt.title("Gamma=0.001")
-	plt.ylim([0.60, 1])
-	plt.legend(['test accuracy', 'train accuracy'], loc='upper left')
-	plt.xscale('log')
-
-
-	# subplot 3/3
-	plt.subplot(133)
-	gamma_0001 = cv_results[cv_results['param_gamma']==0.0001]
-
-	plt.plot(gamma_0001["param_C"], gamma_0001["mean_test_score"])
-	plt.plot(gamma_0001["param_C"], gamma_0001["mean_train_score"])
-	plt.xlabel('C')
-	plt.ylabel('Accuracy')
-	plt.title("Gamma=0.0001")
-	plt.ylim([0.60, 1])
-	plt.legend(['test accuracy', 'train accuracy'], loc='upper left')
-	plt.xscale('log')
-
-	# printing the optimal accuracy score and hyperparameters
-	best_score = model_cv.best_score_
-	best_hyperparams = model_cv.best_params_
-
-	print("The best test score is {0} corresponding to hyperparameters {1}".format(best_score, best_hyperparams))
 
 
 
@@ -157,6 +81,8 @@ if __name__ == '__main__':
 
 	X_train = X_train.reshape((60000,784))
 	X_test = X_test.reshape((10000,784))
+
+	X_test_orig = X_test
 	# print(X_train.shape)
 	# print(X_test.shape)
 
@@ -172,8 +98,7 @@ if __name__ == '__main__':
 	X_train = X_train/255.0
 	X_test = X_test/255.0
 
-	print(X_train.shape)
-	print(X_test.shape)
+
 
 	# X_train = scale(X_train)
 	# X_test = scale(X_test)
@@ -181,28 +106,34 @@ if __name__ == '__main__':
 	X_train = sc.fit_transform(X_train)
 	X_test = sc.transform(X_test)
 
+	
+
 	if args.transform == 'PCA':
-		n_components = 16
+		n_components = 169
 		pca = PCA(n_components=n_components, svd_solver='randomized',
 		          whiten=True)
 		X_train = pca.fit_transform(X_train)
 		X_test = pca.transform(X_test)
 		plt.hist(pca.explained_variance_ratio_, bins=n_components, log=True)
-		plt.savefig(f'transform=PCA_kernel={args.kernel}_hist.png')
+		plt.savefig(f'explained_variance_ratio_hist.png')
 		print("Explained Variance ration:", pca.explained_variance_ratio_.sum())
 		plt.close()
 
 	if args.transform == 'LDA':
-		lda = LDA(n_components=16)
+		lda = LDA(n_components=9)
 		X_train = lda.fit_transform(X_train, y_train)
 		X_test = lda.transform(X_test)
 
-
-
+	print(X_train.shape)
+	print(X_test.shape)
+	# X_test__ = X_test.reshape(X_test.shape[0], int(np.sqrt(X_test.shape[1])),int(np.sqrt(X_test.shape[1])))
+	# print(X_test__.shape[1])
+	# X_test__ = X_test_orig.reshape(X_test.shape[0], 28, 28)
+	# print(X_test__.shape)
 
 	C= 'default'
 	gamma = 'default'
+	# C=10
+	# gamma=0.001
 
-	# svm_model(X_train, y_train, X_test, y_test, args.kernel,args.transform, C, gamma)
-	hyperparameter_tuning(X_train, y_train, X_test, y_test, args.kernel)
-
+	svm_model(X_train, y_train, X_test, y_test, args.kernel,args.transform, C, gamma, X_test_orig)
